478bb7f5715e09d840e0e29f783b386fbc2b6a67
MatheusMarquesEiras <matheusmarqueseiras@gmail.com>
2025-06-04 16:35:13 -0300

    routes init

--- main.py
+++ main.py
@@ -3,15 +3,31 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/register', methods=['POST'])
-def create_user():
-    print()
+def register():
+    try:
+        data = request.json
+        username = data["username"]
+        password = data["password"]
+
+        return jsonify({'message': 'deu boa', "username": username, "password": password})
+    
+    except:
+        return jsonify({"message": "deu ruim"})
 
 @app.route('/login', methods=['POST'])
-def create_user():
-    print()
+def login():
+    try:
+        data = request.json
+        username = data["username"]
+        password = data["password"]
+
+        return jsonify({'message': 'deu boa', "username": username, "password": password})
+    
+    except:
+        return jsonify({"message": "deu ruim"})
 
 @app.route('/get_data', methods=['POST'])
-def create_user():
-    print()
+def get_data():
+    return 'b'
 
-app.run()
+app.run(port=8890)
